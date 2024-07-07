76e0cdf2e0b39d8ffc2554eda2a3aa5b332a9021
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-07-07 15:20:05 -0500

    Add TODO to variables2.rs.

--- exercises/01_variables/variables2.rs
+++ exercises/01_variables/variables2.rs
@@ -1,9 +1,5 @@
-// variables2.rs
-//
-// Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
-// hint.
-
 fn main() {
+    // TODO: Change the line below to fix the compiler error.
     let x: i32 = 10;
     if x == 10 {
         println!("x is ten!");
